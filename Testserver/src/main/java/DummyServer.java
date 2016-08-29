81977b7553bf4ea711c20352cf79f730430031c6
Srikar Reddy <srikar.nr@gmail.com>
2016-08-29 19:21:23 +0200

    Added a sample Test Dropwizard Server

---
+++ Testserver/src/main/java/DummyServer.java
@@ -0,0 +1,25 @@
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+
+/**
+ * Created by Srikar on 29-08-2016.
+ */
+public class DummyServer extends Application<Configuration>{
+
+    @Override
+    public void initialize(Bootstrap<Configuration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(Configuration configuration, Environment environment) throws Exception {
+        environment.jersey().register(DummyResource.class);
+    }
+
+    public static void main(String[] args) throws Exception{
+        new DummyServer().run(args);
+    }
+}
