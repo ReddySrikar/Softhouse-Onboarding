94079e267155642e9308bb7ccb8b7af019eb456d
Srikar Reddy <srikar.nr@gmail.com>
2016-08-31 10:40:22 +0200

    Added Server Configuration

---
+++ Testserver/src/main/java/DummyResource.java
@@ -0,0 +1,19 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by Srikar on 29-08-2016.
+ */
+
+@Path("")
+@Produces(MediaType.APPLICATION_JSON)
+public class DummyResource {
+    @GET
+    public String getWords(){
+        return "Hello World," +
+                "Welcome... " +
+                "To the first dropwizard Program";
+    }
+}
